0ef899825bb80dab28cee4f8b8d548c5b1a7e724
Safi Ullah <dev.safi.96@gmail.com>
2024-12-10 17:46:58 +0500

    frontend/added minor type fix

--- src/app/page.js
+++ src/app/page.js
@@ -72,7 +72,7 @@ const Home = () => {
             type="submit"
             className="w-full py-2 px-4 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            Login and new
+            Login
           </button>
         </form>
         <br></br>
