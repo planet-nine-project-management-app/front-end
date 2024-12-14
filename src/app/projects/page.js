7ab90c3f73a63f1eccaf8edbec3177672c221830
Safi Ullah <dev.safi.96@gmail.com>
2024-12-14 11:59:48 +0500

    Added: added projects styling

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -10,41 +10,45 @@ const ProjectsPage = () => {
             style={{ backgroundImage: 'url(/projects.png)' }}
           >
             <header className="w-full flex justify-between items-center px-8 py-4">
-              <div className="flex items-center space-x-4">
-                <button className="w-70 h-10 rounded-full">
-                  <span className="full text-white">البحث</span>
-                </button>
-                <button className="w-80 h-10 rounded-full bg-green-400">
-                  <span className="text-white">هلا اسم التعريف</span>
-                </button>
+              <div className="w-[95%] mx-auto border-b border-white">
+                <div className="flex justify-between mb-4 items-center">
+                  <div className="flex items-center space-x-4">
+                    <button className="group flex items-center justify-center w-10 h-10 text-white rounded-full border-solid overflow-hidden transition-all duration-300 hover:w-60 border-[1px] border-white">
+                      <span className="opacity-0 ml-2 text-white transition-opacity duration-300 group-hover:opacity-100">البحث</span>
+                    </button>
+                    <button className="w-60 h-10 rounded-full bg-green-400">
+                      <span className="text-white">هلا اسم التعريف</span>
+                    </button>
+                  </div>
+                  <nav className="flex space-x-4 text-right">
+                    <Link href="/" className="hover:underline text-gray-200">
+                      داشبور
+                    </Link>
+                    <Link href="/management" className="hover:underline text-gray-200">
+                      لوحات الإدارة
+                    </Link>
+                    <Link href="/logout" className="hover:underline text-gray-200">
+                      الخروج
+                    </Link>
+                    <Link href="/projects" className="hover:underline text-white">
+                      المشاريع
+                    </Link>
+                  </nav>
+                </div>
               </div>
-              <nav className="flex space-x-4 text-right">
-                <Link href="/" className="hover:underline text-gray-200">
-                  داشبور
-                </Link>
-                <Link href="/management" className="hover:underline text-gray-200">
-                  لوحات الإدارة
-                </Link>
-                <Link href="/logout" className="hover:underline text-gray-200">
-                  الخروج
-                </Link>
-                <Link href="/projects" className="hover:underline text-white">
-                  المشاريع
-                </Link>
-              </nav>
             </header>
             <div className="flex items-center justify-center h-full space-x-4">
-              <div className="hidden md:flex flex-col text-right justify-center w-1/4 p-8 rounded-1.5rem border-[1px] border-white border-solid text-white">
+              <div className="hidden md:flex flex-col text-right justify-center w-1/4 p-8 rounded-1.5rem border-[1px] border-white border-solid text-white hover:bg-gray-100 hover:text-black hover:cursor-pointer transition duration-300">
                 <h2 className="text-lg font-semibold">المشروع الثالث</h2>
                 <button className="text-green-500 mt-4">{'←'}</button>
                 <p className="text-sm">الوصف</p>
               </div>
-              <div className="flex flex-col text-right justify-center w-1/4 bg-white p-8 rounded-1.5rem shadow-md">
+              <div className="flex flex-col text-right justify-center w-1/4 bg-white p-8 rounded-1.5rem shadow-md hover:bg-gray-100 transition duration-300 hover:cursor-pointer">
                 <h2 className="text-lg font-semibold">المشروع الثاني</h2>
                 <p className="text-sm">الوصف</p>
                 <button className="text-green-500 mt-4">{'←'}</button>
               </div>
-              <div className="hidden md:flex flex-col text-right justify-center w-1/4 p-8 rounded-1.5rem border-[1px] border-white border-solid text-white">
+              <div className="hidden md:flex flex-col text-right justify-center w-1/4 p-8 rounded-1.5rem border-[1px] border-white border-solid text-white hover:bg-gray-100 hover:text-black hover:cursor-pointer transition duration-300">
                 <h2 className="text-lg font-semibold">مشروع التميز المؤسسي</h2>
                 <p className="text-sm">الوصف</p>
                 <button className="text-green-500 mt-4">{'←'}</button>
