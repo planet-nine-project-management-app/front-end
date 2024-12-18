bd20283ef5576594c03b1445e6d7ece06b77415b
Safi Ullah <dev.safi.96@gmail.com>
2024-12-18 23:36:05 +0500

    Added login page pixel perfect styling

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -32,7 +32,7 @@ const Home = () => {
       );
 
       localStorage.setItem('token', response.data.token);
-      router.push('/');
+      router.push('/projects');
     } catch (err) {
       setError(err.response?.data?.message || 'An error occurred');
     }
@@ -40,27 +40,27 @@ const Home = () => {
 
   return (
     <div className="flex justify-center items-center min-h-screen p-4">
-      <div className="flex flex-col md:flex-row w-full max-w-5xl bg-white rounded-1.5rem shadow-lg overflow-hidden" style={{ height: 'calc(-250px + 95vh);' }}>
+      <div className="flex flex-col md:flex-row w-[75%] bg-white rounded-1.5rem shadow-lg overflow-hidden" style={{ height: 'calc(-210px + 95vh);' }}>
         <div className="hidden md:flex w-full md:w-1/2 bg-cover bg-center" style={{ backgroundImage: 'url(login-page-image.png)' }}></div>
-        <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-8">
-          <h1 className="text-3xl font-semibold  text-right w-full">!هلا، إسم التعريف</h1>
-          <p className='mb-6 text-right w-full'>الرجاء إدخال المعلومات الخاصة </p>
+        <div className="flex flex-col justify-center items-center w-full md:w-1/2 p-20">
+          <h1 className="text-[2.25rem] leading-[4.5rem] font-semibold text-gray-600 text-right w-full">!أهلا، إسم التعريف</h1>
+          <p className='mb-6 text-right w-full text-[25px] font-bold text-gray-500'>الرجاء إدخال المعلومات الخاصة </p>
           <form onSubmit={handleSubmit} className="space-y-6 w-full max-w-md">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium text-right">اسم التعريف</label>
+            <div className='mt-8'>
+              <label htmlFor="email" className="block text-[1.25rem] font-bold text-gray-600 text-right">اسم التعريف</label>
               <input
                 id="email"
                 type="email"
-                placeholder="اكتب إيميلك هنا"
+                placeholder="أدخل البريد ا@لكتروني"
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 required
-                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md text-right"
+                className="placeholder:text-[1.25rem] placeholder:font-bold placeholder:text-gray-500 mt-2 block w-full px-4 py-2 border border-gray-300 rounded-[2.375rem] h-[80px] text-right"
               />
             </div>
 
-            <div>
-              <label htmlFor="password" className="block text-sm font-medium text-right">كلمة السر</label>
+            <div className='mt-4'>
+              <label htmlFor="password" className="block text-[1.25rem] font-bold text-gray-600 text-right">كلمة السر</label>
               <input
                 id="password"
                 type="password"
@@ -68,20 +68,20 @@ const Home = () => {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 required
-                className="mt-2 block w-full px-4 py-2 border border-gray-300 rounded-md text-right"
+                className="placeholder:text-[1.25rem] placeholder:font-bold placeholder:text-gray-500 mt-2 block w-full px-4 py-2 border border-gray-300 rounded-[2.375rem] h-[80px] text-right"
               />
             </div>
 
-            <div className="flex justify-between items-center">
-              <Link href="/forgot-password" className="text-sm text-green-600 hover:underline">نسيت كلمة السر</Link>
-              <label className="text-sm">
-                <input type="checkbox" className="mr-2" /> تذكر
+            <div className="flex justify-between items-center mt-12">
+              <Link href="/forgot-password" className="text-[1.25rem] font-bold text-gray-500 hover:underline">نسيت كلمة السر</Link>
+              <label className="text-[1.25rem] text-gray-500">
+                <input type="checkbox" className="mr-2" />تذكر
               </label>
             </div>
 
             <button
               type="submit"
-              className="w-full h-[50px] py-2 px-4 bg-green-400 text-white rounded-[40px] hover:bg-green-300"
+              className="w-full h-[80px] font-bold py-2 px-4 bg-green-400 text-white rounded-[40px] hover:bg-green-300 mt-4"
             >
               الدخول
             </button>
